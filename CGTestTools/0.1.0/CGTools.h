b0a61bfc1541030f5d93a9f404f5ce057560cee8
iOS-陈刚 <1204507385@qq.com>
2021-04-06 15:01:10 +0800

    0.1.0

---
+++ CGTestTools/0.1.0/CGTools.h
@@ -0,0 +1,17 @@
+//
+//  CGTools.h
+//  CGTools_Example
+//
+//  Created by chengang on 2021/4/6.
+//  Copyright © 2021 1204507385@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CGTools : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
